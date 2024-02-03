c5f92c0b68324484b8ca4ce81ace597d89404808
AzeemKhan <azeemkh528@gmail.com>
2024-02-03 09:41:31 +0530

    new commit

---
+++ headers in Cpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "header.h"
+
+using namespace std;
+
+int main(){
+    int value1 = 50;
+    int value2 = 120;
+
+    cout << addvalue(value1 , value2) << endl;
+
+    return 0;
+}
