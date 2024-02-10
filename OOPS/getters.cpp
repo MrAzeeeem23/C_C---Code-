4db1e93d397eb28b9bbe17017ed90d42a53a2df0
AzeemKhan <azeemkh528@gmail.com>
2024-02-10 10:14:44 +0530

    small modification done

--- OOPS/getters.cpp
+++ OOPS/getters.cpp
@@ -9,19 +9,38 @@ class myuser{
 
     public:
     string name = "hello world";
-    void func(){
-        cout << "print massage: " << name << endl;
-    }
+    void func()
+    // {
+    //     // cout << "print massage: " << name << endl;
+    // }
 
     // this is a setter
+    // "const" is a keyword use to consider it to be constant.
     void setuser(const int &newscreat){
         _Secret = newscreat;
     }
     // this is getter
-    int getuser(){
+
+    int mylog()const;
+
+    int getuser()/*const*/{
+        // we can use const to give access to the const object.
         return Secret;
     }
 };
+// if in case we wnat to access class method outside of the class or it is not an object of a class.
+// we use some of the sentatic sugar to access them
+
+// First we have to use the class name and :: this sign and method name. to access them.
+// NOTE- functions used in a class are said to be methods.
+void myuser::func(){
+        cout << "print massage: " << name << endl;
+}
+
+// if a method in a class have only const and it is not returning anything use have to use again sentatical sugar.
+int myuser::mylog()const {
+    cout << "hello i am method from class"
+}
 
 int main(){
     myuser ankit;
@@ -34,3 +53,10 @@ int main(){
     return 0;
 
 }
+
+// ---------NOTE------------//
+/*
+Is is most common to use constant objects but their is a catch
+in order to access constant method in side a class we use const keyword in the getter method
+non constant object can access const methods but constant objects cannot access non constent methods.
+*/
